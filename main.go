8e63bc48329d3f37e48ac31b8c60330d3970d4f8
VnioDilol <dilolvnio@gmail.com>
2022-12-11 15:59:03 +0700

    Changed the structure of the main file, to switch to object-oriented programming

--- main.go
+++ main.go
@@ -6,27 +6,41 @@ import (
 	"time"
 )
 
-var Map [30][75]int
-
 func main() {
+	re_x := 25
+	re_y := 10
 
-	rand.Seed(time.Now().UnixNano())
+	var Map [][]int
+	Map = write_rnd(&Map, re_y, re_x)
+
+	drow(Map)
+
+}
+
+func drow(Map [][]int) {
 	correcting_var := (len(Map[0]) - 1)
+	rol := ""
 
 	for y, m := range Map {
 		for x := range m {
-			Map[y][x] = rand.Intn(3)
-
-		}
-	}
-
-	for y, m := range Map {
-		for x := range m {
-			fmt.Print(Map[y][x])
+			rol += fmt.Sprint(Map[y][x])
 			if x == correcting_var {
-				fmt.Println()
-
+				rol += "\n"
 			}
 		}
 	}
+	fmt.Print(rol)
+}
+
+func write_rnd(Map_Start *[][]int, y int, x int) [][]int {
+	rand.Seed(time.Now().UnixNano())
+	Map_S := *Map_Start
+	for i := 0; i < y; i++ {
+		var buff []int
+		for j := 0; j < x; j++ {
+			buff = append(buff, rand.Intn(3))
+		}
+		Map_S = append(Map_S, buff)
+	}
+	return Map_S
 }
